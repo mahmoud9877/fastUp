5e72c7925dd14c6a80a9406b73faab050ecb9a13
mahmoud9877 <mohmoudali9877@gmail.com>
2025-02-27 19:58:03 +0200

    token error

--- src/utils/GenerateAndVerifyToken.js
+++ src/utils/GenerateAndVerifyToken.js
@@ -1,20 +1,21 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = ({
-  payload = {},
-  signature = process.env.TOKEN_SIGNATURE,
-  expiresIn = 60 * 60,
-} = {}) => {
-  const token = jwt.sign(payload, signature, {
-    expiresIn: parseInt(expiresIn),
-  });
-  return token;
+export const generateToken = ({ payload, expiresIn = "1h" }) => {
+  if (!process.env.TOKEN_SIGNATURE) {
+    throw new Error("TOKEN_SIGNATURE is missing from environment variables");
+  }
+
+  return jwt.sign(payload, process.env.TOKEN_SIGNATURE, { expiresIn });
 };
 
-export const verifyToken = ({
-  token,
-  signature = process.env.TOKEN_SIGNATURE,
-} = {}) => {
-  const decoded = jwt.verify(token, signature);
-  return decoded;
+export const verifyToken = (token) => {
+  if (!process.env.TOKEN_SIGNATURE) {
+    throw new Error("TOKEN_SIGNATURE is missing from environment variables");
+  }
+
+  try {
+    return jwt.verify(token, process.env.TOKEN_SIGNATURE);
+  } catch (error) {
+    return null; // Return null instead of throwing an error
+  }
 };
